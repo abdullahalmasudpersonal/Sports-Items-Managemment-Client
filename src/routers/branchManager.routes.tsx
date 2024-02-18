43a5b67850a135fad02806641941321a9b90d773
Abdullah Al Masud <abdullahalmasud01737906772@gmail.com>
2024-02-19 00:40:28 +0600

    add branch manager product

--- src/routers/branchManager.routes.tsx
+++ src/routers/branchManager.routes.tsx
@@ -1,10 +1,32 @@
 import BranchManagerDashboard from "../pages/branchManager/BranchManagerDashboard";
-
+import CreateProduct from "../pages/branchManager/sportsItemsManagements/CreateProduct";
+import DeleteProduct from "../pages/branchManager/sportsItemsManagements/DeleteProduct";
+import GetAllProducts from "../pages/branchManager/sportsItemsManagements/GetAllProducts";
 
 export const branchManagerPaths = [
-    {
-        name: "Dashboard",
-        path: "dashboard",
-        element: <BranchManagerDashboard />,
-      }
-]
+  {
+    name: "Dashboard",
+    path: "dashboard",
+    element: <BranchManagerDashboard />,
+  },
+  {
+    name: "Sports Items Management",
+    children: [
+      {
+        name: "Create Product",
+        path: "sports-items-management/create-product",
+        element: <CreateProduct />,
+      },
+      {
+        name: "All Products",
+        path: "sports-items-management/products",
+        element: <GetAllProducts />,
+      },
+      {
+        name: "Delete Products",
+        path: "sports-items-management/delete-product",
+        element: <DeleteProduct />,
+      },
+    ],
+  },
+];
